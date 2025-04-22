3a0999a339dbe01c7367eedd86457b70e1c335cc
ratul9052 <ratuldas.9052@gmail.com>
2025-04-22 21:37:05 +0600

    first commit

---
+++ 1D heat/1D_Heat_FDM.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+# 1D Heat equation
+# u_t = c^2 * u_xx domain (0,pi)
+# u(x,0) = sin(x)
+# u(0,t) = u(L,t) = 0
+
+#physical domain
+g=20
+n=100
+T=10
+x=np.linspace(0,np.pi,g)
+t=np.linspace(0,T,n)   
+U=[]
+
+#analytical soluation for c=1
+def analytical_solution(x,t):
+    u=np.sin(x)*np.exp(-t)
+    return u
+
+
+#neumerical soluation
+u_0=np.sin(x)  #initial condition
+dx=x[1]-x[0]
+dt=0.01
+T=10
+
+print("cfl:", dt/dx)
+n_sol=[]
+a_sol=[]
+
+
+n_sol.append(u_0)
+a_sol.append(u_0)
+a=0
+while a<T:
+    u_old=n_sol[-1]
+    u_new=np.zeros_like(u_old)
+
+    u_new[1:-1]=u_old[1:-1] + ((dt/(dx)**2) * (u_old[2:] - 2*u_old[1:-1] + u_old[:-2]))
+
+    
+    u_new[0]=0
+    u_new[-1]=0
+    n_sol.append(u_new)
+    a_sol.append(analytical_solution(x,a))
+    a+=dt
+
+
+
+
+
+#PLOTING
+ims = []
+fig = plt.figure(figsize=[5,4], dpi=200)
+plt.grid()
+i = 0
+for n in range(len(n_sol)):
+
+    im1 = plt.plot(x,a_sol[n-1], 'x', color='b', markersize=2, animated=True)
+    im2 = plt.plot(x,n_sol[n-1], '-', color='r', markersize=2, animated=True)
+    #plt.ylim(-1.1, 1.1)
+    ims.append(im2 + im1)
+
+
+ani = animation.ArtistAnimation(fig, ims, interval=35, blit=True, repeat_delay=1000)
+ani
+
+#plt.plot(x,n_sol[3], '-o', color='r', markersize=2)
+
+plt.show()
+
+
+
+
